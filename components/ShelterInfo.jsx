ce05c7e67cd35b6a8c03588f9a226d7c4885985a
Edrian Hernandez <117151322+EdrianHernandez@users.noreply.github.com>
2026-01-11 23:00:16 +0800

    Change ShelterInfo component from TypeScript to JavaScript

--- components/ShelterInfo.jsx
+++ components/ShelterInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ShelterInfo: React.FC = () => {
+const ShelterInfo = () => {
   return (
     <footer className="bg-slate-900 text-slate-300 pt-20 pb-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
